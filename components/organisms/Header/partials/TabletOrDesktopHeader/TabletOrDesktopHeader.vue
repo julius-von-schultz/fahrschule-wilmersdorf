7107ecad64357f4800577ab154d5320c068e41d0
julius-von-schultz <julius.schultz@diva-e.com>
2025-03-22 12:57:41 +0100

    add pages, add color scheme

---
+++ components/organisms/Header/partials/TabletOrDesktopHeader/TabletOrDesktopHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <ContentWrapper class="header__grid">
+      TEst
+    </ContentWrapper>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.header {
+  @apply block;
+  @apply bg-fw-blue;
+}
+</style>
